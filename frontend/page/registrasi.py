9a2397fa763c4c0ad59595c085bd1ccf4e884684
yonii <mahayoni01@gmail.com>
2026-06-09 23:41:25 +0800

    Delete

--- frontend/page/registrasi.py
+++
@@ -1,9 +0,0 @@
-import streamlit as st
-
-st.title("Register")
-
-username = st.text_input("Username Baru")
-password = st.text_input("Password Baru", type="password")
-
-if st.button("Daftar"):
-    st.success("Akun berhasil dibuat")
